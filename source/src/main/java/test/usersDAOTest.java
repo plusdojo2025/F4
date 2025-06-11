1398252aa2675c9b113407b56cfdb67d630b7e70
plusdojo2025-sadaka <sadaka-ryutaro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 15:30:55 +0900

    test ファイル

---
+++ source/src/main/java/test/usersDAOTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import dao.usersDAO;
+import dto.usersDTO;
+
+public class usersDAOTest {
+	public static void main(String[] args) {
+		testIsLoginOK1(); // ユーザーが見つかる場合のテスト
+		testIsLoginOK2(); // ユーザーが見つからない場合のテスト
+	}
+
+	// ユーザーが見つかる場合のテスト
+	public static void testIsLoginOK1() {
+		usersDAO dao = new usersDAO();
+		if (dao.isLoginOK(new usersDTO(0, "", "tanaka.taro@example.jp", "taro1234"))) {
+			System.out.println("testIsLoginOK1：テストが成功しました");
+		} else {
+			System.out.println("testIsLoginOK1：テストが失敗しました");
+		}
+	}
+
+	// ユーザーが見つからない場合のテスト
+	public static void testIsLoginOK2() {
+		usersDAO dao = new usersDAO();
+		if (!dao.isLoginOK(new usersDTO(0, "", "mail", "pw"))) {
+			System.out.println("testIsLoginOK2：テストが成功しました");
+		} else {
+			System.out.println("testIsLoginOK2：テストが失敗しました");
+		}
+	}
+}
