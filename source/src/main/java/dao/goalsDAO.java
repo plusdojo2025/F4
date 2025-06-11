a410a9e7768a9c3744e3702bbf73a4fc4f6006b9
plusdojo2025-sugahara <sugahara-fumika-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 15:26:19 +0900

    goalsDAO作成

---
+++ source/src/main/java/dao/goalsDAO.java
@@ -0,0 +1,82 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.goalsDTO;
+
+
+public class goalsDAO {
+
+	// 目標を登録
+	public void insert(goalsDTO goal) {
+		try {
+			// データベースに接続する
+			Connection conn = dbConnectionDAO.getConnection();
+			
+			// INSET文を準備する
+			String sql = "INSERT INTO goals VALUES(0, ?, ?, ?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, goal.getId());
+		    pStmt.setDouble(2, goal.getExercise_goal());
+			pStmt.setDouble(3, goal.getStudy_goal());
+			pStmt.setDouble(4, goal.getSleep_goal());
+
+			// SQL文を実行する
+			pStmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	// ユーザーの目標を取得
+	public List<goalsDTO> select(int userId) {
+		List<goalsDTO> goalList = new ArrayList<>();
+		try {
+			// データベースに接続する
+			Connection conn = dbConnectionDAO.getConnection();
+				
+			// INSET文を準備する
+			String sql = "SELECT * FROM goals WHERE id = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			pStmt.setInt(1, userId);
+            ResultSet rs = pStmt.executeQuery();
+
+			
+			while (rs.next()) {
+				
+				goalsDTO goal = new goalsDTO(
+						rs.getInt("goals_id"),
+						rs.getInt("id"),
+						rs.getDouble("exercise_goal"),
+						rs.getDouble("study_goal"),
+						rs.getDouble("sleep_goal")
+						);
+				
+				goalList.add(goal);
+			}
+  
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		
+		return goalList;
+	}
+}
+
+	
+	
+
+
+
+
+
