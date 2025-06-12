ae12d29098222a5c24ae39ad9d3b6fb5446b9353
plusdojo2025-yamada-koei <yamada-kouei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 16:27:24 +0900

    ホーム画面のサーブレット

--- source/src/main/java/servlet/homeServlet.java
+++ source/src/main/java/servlet/homeServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import dao.goalsDAO;
 import dto.goalsDTO;
 import dto.usersDTO;
 
@@ -32,23 +34,44 @@ public class homeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 				
-				//名前を取得
+				//名前を取得セッションからとる
 				usersDTO user = new usersDTO();
 				String name= user.getUser_name();
 				
 				//名前を格納
 		        request.setAttribute("name",name);
 		        
-		        //目標の取得
-		        goalsDTO goal = new goalsDTO();
-		        Double exercise = goal.getExercise_goal();
-		        Double study = goal.getStudy_goal();
-		        Double sleep = goal.getSleep_goal();
+		        //ユーザーのIDの取得
+		        request.setCharacterEncoding("UTF-8");
+		        	HttpSession session = request.getSession();
+		        	Integer userId = (Integer)(session.getAttribute("user_id"));
+		 
+		        //実施時間を格納する変数の宣言
+		        	 Double exercise_do;
+		        	 Double  study_do;
+		        	 Double  sleep_do;
+		        	 
+		        if(userId != null) {
+			        //目標の取得
+			        goalsDAO gDao = new goalsDAO();
+			        goalsDTO goal = gDao.selectGoal(userId);
+			        
+			        exercise_do = goal.getExercise_goal();
+			        study_do = goal.getExercise_goal();
+			        sleep_do = goal.getExercise_goal();
+		        }else {
+		        	//ユーザーのIDを取得できなかった時の処理
+		        	exercise_do = (double)0;
+		        	study_do = (double)0;
+		        	sleep_do = (double)0;
+		        }
+		        
+
 		        
 		        //目標の格納
-		        request.setAttribute("exercise", exercise);
-		        request.setAttribute("study", study);
-		        request.setAttribute("sleep", sleep);
+		        request.setAttribute("exercise_do", exercise_do);
+		        request.setAttribute("study_do", study_do);
+		        request.setAttribute("sleep_do", sleep_do);
 		        
 		        //ホーム画面にフォアード
 		        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
