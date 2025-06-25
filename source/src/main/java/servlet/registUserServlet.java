ad8ea9e8fea25c1376f339ba51ab7e4273150c54
plusdojo2025-sugahara <sugahara-fumika-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-25 14:13:14 +0900

    メールアドレス

--- source/src/main/java/servlet/registUserServlet.java
+++ source/src/main/java/servlet/registUserServlet.java
@@ -54,7 +54,7 @@ public class registUserServlet extends HttpServlet {
 	    }
 	    
 	    // メールアドレス形式チェック
-	    String emailPattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+	    String emailPattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+$";
 
 	    if (!mail.matches(emailPattern)) {
 	        request.setAttribute("errorMessage", "メールアドレスの形式が正しくありません。");
